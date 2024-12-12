d2854694f88d0002623d0745af853844a059e703
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-12 18:57:12 +0100

    pasar sing in a el seu component (html i css) i provar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CondicionesComponent} from './condiciones/condiciones.component';
 import {ContactoComponent} from './contacto/contacto.component';
+import {SignInComponent} from './sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CondicionesComponent, ContactoComponent],
+  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
