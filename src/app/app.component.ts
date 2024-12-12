dd36313cfe3d3f6a3e37e6db2f79641daa5ce6ba
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-12 18:52:58 +0100

    pasar condiciones i contacte a els seus respectius components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CondicionesComponent} from './condiciones/condiciones.component';
+import {ContactoComponent} from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CondicionesComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
