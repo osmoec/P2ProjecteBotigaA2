02a2b3c2c6c84af9e4eca1d25fd2f88f62abae84
AdriG <emaildeadria@gmail.com>
2024-12-12 13:37:31 +0100

    Convertido y adaptado el componente cesta. Retirados estilos repetidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SobrenosotrosComponent} from './sobrenosotros/sobrenosotros.component';
+import {CestaComponent} from './cesta/cesta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SobrenosotrosComponent],
+  imports: [RouterOutlet, SobrenosotrosComponent, CestaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
