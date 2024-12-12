93edcccf9c6901834d77645085adf8f185c6348c
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-12 11:30:51 +0100

    Creacio components

---
+++ src/app/pagament/pagament.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagament',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagament.component.html',
+  styleUrl: './pagament.component.css'
+})
+export class PagamentComponent {
+
+}
