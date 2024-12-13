644c5deeac2827a03d32507cf61784988f294b01
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-13 09:29:07 +0100

    arreglar sistema carpetas

--- src/app/Componentes/app.component.ts
+++ src/app/Componentes/app.component.ts
@@ -3,13 +3,11 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {CondicionesComponent} from './condiciones/condiciones.component';
 import {ContactoComponent} from './contacto/contacto.component';
 import {SignInComponent} from './sign-in/sign-in.component';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, HeaderComponent, FooterComponent, RouterLink],
+  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
