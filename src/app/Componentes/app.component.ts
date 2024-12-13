4ce1e8694173987bbdf2f4e6df6a0d50cb0f4b09
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-13 09:46:11 +0100

    arreglar problema header

--- src/app/Componentes/app.component.ts
+++ src/app/Componentes/app.component.ts
@@ -3,11 +3,12 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {CondicionesComponent} from './condiciones/condiciones.component';
 import {ContactoComponent} from './contacto/contacto.component';
 import {SignInComponent} from './sign-in/sign-in.component';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, RouterLink],
+  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
