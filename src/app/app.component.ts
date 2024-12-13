8c45334660da374ad8ed63a15222a2823af81516
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-13 08:44:18 +0100

    afegir sistema de redirecio de components amb routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {CondicionesComponent} from './condiciones/condiciones.component';
 import {ContactoComponent} from './contacto/contacto.component';
 import {SignInComponent} from './sign-in/sign-in.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent],
+  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
