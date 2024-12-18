18d8fae6503146cf9fbbc6142aeca4de31598198
DavidGelm <david.gelma@institutvidreres.cat>
2024-12-18 19:45:13 +0100

    Canviar logout i usuari al menu amb els seus estils

--- src/app/Componentes/app.component.ts
+++ src/app/Componentes/app.component.ts
@@ -1,19 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {CondicionesComponent} from './condiciones/condiciones.component';
 import {ContactoComponent} from './contacto/contacto.component';
 import {SignInComponent} from './sign-in/sign-in.component';
 import {HeaderComponent} from './header/header.component';
 import {LoginComponent} from "./login/login.component";
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, RouterLink, HeaderComponent, LoginComponent],
+  imports: [RouterOutlet, CondicionesComponent, ContactoComponent, SignInComponent, RouterLink, HeaderComponent, LoginComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'P2ProjecteBotigaA2';
+  logat = false;
+  usuariLogat = '';
+
+  ngOnInit() {
+    this.logat = localStorage.getItem('logat') === 'true';
+    this.usuariLogat = localStorage.getItem('usuari') || '';
+  }
+
+  logout() {
+    localStorage.removeItem('logat');
+    localStorage.removeItem('usuari');
+    this.logat = false;
+    this.usuariLogat = '';
+    window.location.reload();
+  }
 }
--- src/app/Componentes/header/header.component.ts
+++ src/app/Componentes/header/header.component.ts
@@ -1,28 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
-  imports: [NgIf],
+  imports: [],
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  logat = false;
-  usuariLogat = '';
+export class HeaderComponent  {
 
-  ngOnInit() {
-    this.logat = localStorage.getItem('logat') === 'true';
-    this.usuariLogat = localStorage.getItem('usuari') || '';
-  }
-
-  logout() {
-    localStorage.removeItem('logat');
-    localStorage.removeItem('usuari');
-    this.logat = false;
-    this.usuariLogat = '';
-    window.location.reload();
-  }
 }
 
