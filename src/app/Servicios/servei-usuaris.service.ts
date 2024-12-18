948db0c2ac63c8d970cfd0173ccc5a0e4ed2fb70
AdriG <emaildeadria@gmail.com>
2024-12-18 14:09:02 +0100

    añadida pagina bienvenida de usuario

--- src/app/Servicios/servei-usuaris.service.ts
+++ src/app/Servicios/servei-usuaris.service.ts
@@ -6,27 +6,46 @@ import { Usuario } from '../Clases/Usuario.model';
 })
 export class ServeiUsuarisService {
   private usuaris: Usuario[] = [];
-  usuari_logat = ''
+  usuari_logat = '';
+
   constructor() {
     const usuarisGuardats = localStorage.getItem('usuaris');
     if (usuarisGuardats) {
-      this.usuaris = JSON.parse(usuarisGuardats);
+      const datos = JSON.parse(usuarisGuardats); // Parsear JSON desde localStorage
+      this.usuaris = datos.map((data: any) =>
+        new Usuario(
+          data.nombre,
+          data.apellido,
+          data.correo,
+          data.usuario,
+          data.DNI,
+          new Date(data.cumpleaños), // Convertir cumpleaños a Date
+          data.telefono,
+          data.contrasena,
+          data.direccion
+        )
+      );
+      console.log(this.usuaris)
+
     }
   }
+
   addUsuario(usuari: Usuario): void {
     this.usuaris.push(usuari);
     localStorage.setItem('usuaris', JSON.stringify(this.usuaris));
   }
-  getUsuariLogat(): Usuario | null {
-    const usuari = this.usuaris.find(u => u.usuario === this.usuari_logat);
+
+  getUsuariLogat(usuari_logat : string): Usuario | null {
+    const usuari = this.usuaris.find(u => u.usuario === usuari_logat);
     return usuari || null;
   }
+
   setUsuariLogat(usuari: string): void {
     this.usuari_logat = usuari;
     localStorage.setItem('usuari_logat', usuari);
   }
+
   getUsuarios(): Usuario[] {
     return this.usuaris;
   }
 }
-
