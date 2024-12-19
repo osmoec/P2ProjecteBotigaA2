f0a8501315a44c83d2f4f981fd922a973352abe5
DavidGelm <david.gelma@institutvidreres.cat>
2024-12-19 16:42:32 +0100

    Logica del logout arreglada traspassat del header al menu

--- src/app/Componentes/app.component.ts
+++ src/app/Componentes/app.component.ts
@@ -2,17 +2,36 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {ServeiUsuarisService} from '../Servicios/servei-usuaris.service';
+import {NgIf} from '@angular/common';
+import {Usuario} from '../Clases/Usuario.model';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'P2ProjecteBotigaA2';
-  constructor(private serveiUsuaris: ServeiUsuarisService) {
+  logat = false;
+  usuariLogat : Usuario | null;
+
+  constructor(protected serveiUsuaris: ServeiUsuarisService) {
+    this.usuariLogat = this.serveiUsuaris.usuari_logat
+  }
+  ngOnInit() {
+    console.log(this.serveiUsuaris.usuari_logat)
+    if (this.serveiUsuaris.usuari_logat) {
+      this.logat = true
+    }
+    console.log("usuario logeado? " + this.logat)
+
+  }
+
+  logout() {
+    this.serveiUsuaris.usuari_logat = null
+    window.location.reload();
   }
 }
--- src/app/Componentes/header/header.component.ts
+++ src/app/Componentes/header/header.component.ts
@@ -1,35 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { NgIf } from '@angular/common';
-import {Usuario} from '../../Clases/Usuario.model';
-import {ServeiUsuarisService} from '../../Servicios/servei-usuaris.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
-  imports: [NgIf],
+  imports: [],
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  logat = false;
-  usuariLogat : Usuario | null;
+export class HeaderComponent  {
 
-  constructor(protected serveiUsuaris: ServeiUsuarisService) {
-    this.usuariLogat = this.serveiUsuaris.usuari_logat
-  }
-  ngOnInit() {
-    console.log(this.serveiUsuaris.usuari_logat)
-    if (this.serveiUsuaris.usuari_logat) {
-      this.logat = true
-    }
-    console.log("usuario logeado? " + this.logat)
-
-  }
-
-  logout() {
-    this.serveiUsuaris.usuari_logat = null
-    window.location.reload();
-  }
 
 }
 
