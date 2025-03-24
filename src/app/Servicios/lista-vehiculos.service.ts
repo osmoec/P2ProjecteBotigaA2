9463ea7a30352b88d0f5413d86577bd333afec54
AdriG <emaildeadria@gmail.com>
2025-03-24 11:39:35 +0100

    Cambiado origen de lista coches

--- src/app/Servicios/lista-vehiculos.service.ts
+++ src/app/Servicios/lista-vehiculos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Coche} from '../Clases/Coche.model';
 import {HttpClient} from '@angular/common/http';
+import {Usuario} from '../Clases/Usuario.model';
+import {Comanda} from '../Clases/comanda.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +11,37 @@ export class ListaVehiculosService {
 
   coches: Coche[] = [];
   constructor(private http: HttpClient) {
-    this.leerCochesDesdeArchivo()
+    console.log("Se reseteo el servicio coches")
+    this.cargarDatos()
   }
   getMarcasDestacadas() {
     return this.http.get<any>('/api/cotxes');
   }
 
+  cargarDatos(): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.http.get<any>(`http://localhost:3080/db/cotxes`)
+        .subscribe(
+          (response) => {
+            response.forEach((car : any)=> {
+              const coche = new Coche(
+                car.id,
+                car.name,
+                car.price,
+                car.tags,
+                car.offertext,
+                car.imgC
+              )
+              this.coches.push(coche)
+            });
 
-  leerCochesDesdeArchivo() {
-    this.http.get<Coche[]>('/json/cochesCatalogo.json')
-      .subscribe({
-        next: (data) => {
-          this.coches = data.map(obj => new Coche(
-            obj.id, obj.name, obj.price, obj.tags, obj.offertext, obj.imgC
-          ));
-          console.log("coche añadido")
-          console.log(this.coches)
-        },
-        error: (err) => console.error('Error cargando el archivo JSON', err)
-      });
+              resolve(true);
+          },
+          (error) => {
+            console.error('❌ Error en la petición:', error);
+            resolve(false);
+          }
+        );
+    });
   }
 }
