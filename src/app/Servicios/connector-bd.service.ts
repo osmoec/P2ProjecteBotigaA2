97897926acb3b1f8350ab615a1d5ac2234c1a207
DavidGelm <david.gelma@institutvidreres.cat>
2025-03-26 11:52:22 +0100

    Historial productes inicial

--- src/app/Servicios/connector-bd.service.ts
+++ src/app/Servicios/connector-bd.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import{HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectorBDService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+
+  registrarFactura(factura:any):Observable<any> {
+    return this.http.post('http://localhost:3080/historial/afegir-factura-detall', factura);
+  }
 }
