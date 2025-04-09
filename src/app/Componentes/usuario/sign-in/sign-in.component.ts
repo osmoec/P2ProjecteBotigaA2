54702db2c14c2094ede1e70b2d00264313256245
AdriG <emaildeadria@gmail.com>
2025-04-09 08:58:49 +0200

    Reparado registro

--- src/app/Componentes/usuario/sign-in/sign-in.component.ts
+++ src/app/Componentes/usuario/sign-in/sign-in.component.ts
@@ -6,6 +6,7 @@ import { ServeiUsuarisService } from '../../../Servicios/servei-usuaris.service'
 import { NgIf } from '@angular/common';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import {RecaptchaModule} from 'ng-recaptcha';
+import {direction} from 'html2canvas/dist/types/css/property-descriptors/direction';
 
 @Component({
   selector: 'app-sign-in',
@@ -14,7 +15,7 @@ import {RecaptchaModule} from 'ng-recaptcha';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
   @ViewChild('miInput') miInputRef!: ElementRef;
 
   nom: string = '';
@@ -32,21 +33,6 @@ export class SignInComponent implements OnInit {
 
   constructor(private router: Router, private serveiUsuaris: ServeiUsuarisService, public http: HttpClient) {}
 
-  ngOnInit() {
-    // Agregar el event listener para el mensaje desde el iframe de Doom
-    window.addEventListener('message', this.receiveMessage.bind(this), false);
-  }
-
-  receiveMessage(event: MessageEvent) {
-    if (event.origin !== 'https://doom-captcha.vercel.app') return;
-
-    console.log('Mensaje recibido:', event.data);
-
-    // Si el mensaje es un string que contiene el texto de verificación
-    if (typeof event.data === 'string' && event.data.includes('CAPTCHA verified successfully')) {
-      this.captchaCompletado = true;  // El CAPTCHA se ha completado correctamente
-    }
-  }
 
   public validarDatos(): boolean {
     // Validaciones para los campos del formulario
@@ -77,6 +63,7 @@ export class SignInComponent implements OnInit {
 
     // Validación de datos del formulario
     if (this.validarDatos()) {
+      this.serveiUsuaris.addUsuario(new Usuario(this.nom, this.cognom, this.correu, this.usuari, this.DNI, this.aniversari, this.telefon, this.contrasena, this.adreca))
       alert(`Benvingut/da, ${this.nom}!`);
       // Redirigir a la página de login
       this.router.navigate(['/login']);
